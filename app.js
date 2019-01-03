166eb96f0209267846765dfb20d76e02bc0f6663
Sithiz <Joshklaas1@gmail.com>
2019-01-03 15:52:50 -0600

    got some sleep functionality working

--- app.js
+++ app.js
@@ -2,27 +2,31 @@
 
 class petToma {
 	constructor(){
-		this.name = carl
-		this.age = ageFunction()
-		this.hunger = hungerFunction()
-		this.sleepiness = sleepFunction()
-		this.boredom = boredomFunction()
+		this.name = 'carl',
+		this.age = 0,
+		this.hunger = 1,
+		this.sleepiness = 1,
+		this.boredom = 1
 		
 	}
 }
 
+let pet = new petToma 
 
+console.log(pet.hunger,pet.age,pet.name,pet.sleepiness,pet.boredom)
 
+let feed = function(){
+	pet.hunger = 0
+}
+let play = function(){
+	pet.sleepiness = 0
+}
+let light = function(){
+	pet.sleepiness = 0 
+	$('.gizmo').hide(5000)
+}
 
-
-
-
-
-
-
-
-
-
+$('#play').click(light)
 
 
 
