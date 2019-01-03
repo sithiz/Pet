164c74cc3e53b3b3273b5e10dafe40b73ce47c63
Sithiz <Joshklaas1@gmail.com>
2019-01-03 13:50:30 -0600

    #place holder

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+class petToma {
+	constructor(){
+		this.name = carl
+		this.age = ageFunction()
+		this.hunger = hungerFunction()
+		this.sleepiness = sleepFunction()
+		this.boredom = boredomFunction()
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
